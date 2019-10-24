d2d4827b854c83e0b3e78a606f7f8722e52db489
andrea9llo <andrea.novello93@gmail.com>
2019-10-24 17:28:08 +0200

    stampa su pagina

--- js/main.js
+++ js/main.js
@@ -7,18 +7,25 @@
 // dichiaro le variabili
 var lista;
 
+
+
 // creo un ciclo for per stampare numeri da 1 a 100;
 for (var i = 1; i <= 100; i++) {
   // console.log(i);
+  lista = document.getElementById('listanum').innerHTML;
+  
+  // dichiaro le condizioni per i multipli
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i,"FizzBuzz");
+    document.getElementById('listanum').innerHTML = lista + "<li>" + "FizzBuzz" + "</li>";
+    // console.log(i,"FizzBuzz");
   } else if (i % 3 === 0) {
-    console.log(i,"Fizz");
+    document.getElementById('listanum').innerHTML = lista + "<li>" + "Fizz" + "</li>";
+    // console.log(i,"Fizz");
   } else if (i % 5 === 0) {
-    console.log(i,"Buzz");
+    document.getElementById('listanum').innerHTML = lista + "<li>" + "Buzz" + "</li>";
+    // console.log(i,"Buzz");
   } else {
-    console.log(i);
+    document.getElementById('listanum').innerHTML = lista + "<li>" + i + "</li>";
+    // console.log(i);
   }
 }
-// lista = document.getElementById('listanum').innerHTML;
-// document.getElementById('listanum').innerHTML = lista + "<li>" + i + "</li>";
